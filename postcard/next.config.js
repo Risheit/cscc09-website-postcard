f98fe21d1c34201ebc35324234b1360c1f7f6fac
Gigi <106707569+gcheang@users.noreply.github.com>
2024-11-06 14:35:01 -0500

    Downgrade nextjs and react, add konva

---
+++ postcard/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make Konva & react-konva work
+    return config;
+  },
+};
+
+module.exports = nextConfig;
