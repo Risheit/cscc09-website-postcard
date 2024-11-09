7e3b90b65e58e6c071041eafd525b0f2f0baf620
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2024-11-09 12:51:07 -0500

    Add basic backend

---
+++ postcard/src/backend/cloudsql.ts
@@ -0,0 +1,21 @@
+import { Connector, IpAddressTypes } from '@google-cloud/cloud-sql-connector';
+import { Pool } from 'pg';
+
+const connector = new Connector();
+const clientOpts = await connector.getOptions({
+  instanceConnectionName: process.env.DATABASE_CONNECTION_NAME!,
+  ipType: IpAddressTypes.PUBLIC,
+});
+
+const pool = new Pool({
+  ...clientOpts,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export default pool;
