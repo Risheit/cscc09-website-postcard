6120a1e6d36086730a46f3a6a04d071106317926
Risheit Munshi <Risheit.Munshi@mail.utoronto.ca>
2024-11-10 20:33:35 -0500

    Try hardcoding cloudsql connection

--- postcard/src/backend/cloudsql.ts
+++ postcard/src/backend/cloudsql.ts
@@ -2,17 +2,16 @@ import { Connector, IpAddressTypes } from '@google-cloud/cloud-sql-connector';
 import { Pool } from 'pg';
 
 async function getPool() {
-  console.log(JSON.stringify({ hey: 'hey'}));
-  console.log('hey');
-  const connector = new Connector();
-  const clientOpts = await connector.getOptions({
-    instanceConnectionName: 'postcard-439817:me-central1:postcard-db',
-    ipType: IpAddressTypes.PUBLIC,
-  });
-  console.log('clientOpts', clientOpts);
+  // console.log(JSON.stringify({ hey: 'hey'}));
+  // console.log('hey');
+  // const connector = new Connector();
+  // const clientOpts = await connector.getOptions({
+  //   instanceConnectionName: 'postcard-439817:me-central1:postcard-db',
+  //   ipType: IpAddressTypes.PUBLIC,
+  // });
+  // console.log('clientOpts', clientOpts);
   const pool = new Pool({
-    ...clientOpts,
-    host: process.env.DATABASE_HOST,
+    host: '/cloudsql/postcard-439817:me-central1:postcard-db',
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
